e06b412d6220f98ec71effd9e245e259896ee23b
Dimas Adiluhur <99548058+arxycth@users.noreply.github.com>
2025-02-06 23:03:35 +0700

    Add files via upload

---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const create = temp => {
+    const request = axios.post(baseUrl, temp)
+    return request.then(response => response.data)
+}
+
+const deleteSome = id => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => response.data)
+}
+
+const update = (temp,id) => {
+    const request = axios.put(`${baseUrl}/${id}`, temp)
+    return request.then(response => response.data)
+}
+
+export default {getAll, create, deleteSome, update}
